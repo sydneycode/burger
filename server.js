88b08d60e77e36e96b0170fbe6194295da14d25e
sydneycode <sstaggs12@gmail.com>
2018-09-26 00:02:05 -0700

    finished the app setup, including requiring express and body-parser in the server file

---
+++ server.js
@@ -0,0 +1,7 @@
+// Require the npm express package (in order to use the express 
+// web framework for node)
+var express = require("express");
+
+// Require the npm body-parser package (in order to parse request 
+// bodies in a middleware before they are passed to handlers)
+var bodyParser = require("body-parser");
