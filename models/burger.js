693f86f3878b9de09743dce1ef09e98048bd858b
sydneycode <sstaggs12@gmail.com>
2018-09-26 13:31:10 -0700

    created the burger model, including versions of the orm methods as they apply to the burger model

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+// Import ORM into the burger model
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(col, val, cb) {
+        orm.insertOne("burgers", col, val, function(res) {
+            cb(res);
+        })
+    },
+    updateOne: function(col, val, condition, cb) {
+        orm.updateOne("burgers", col, val, condition, function(res) {
+            cb(res);
+        })
+    }
+}
+
+// Export the burger model
+module.exports = burger;
