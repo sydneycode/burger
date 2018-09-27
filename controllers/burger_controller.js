3d493f3f15011d9647d65d6b076685dcbe681ee2
sydneycode <sstaggs12@gmail.com>
2018-09-26 21:58:12 -0700

    worked on the index.handlebars page

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -34,7 +34,7 @@ router.put("/api/burgers/:id", function(req, res) {
   
     console.log("condition", condition);
   
-    cat.update("devoured", "true", condition, function(result) {
+    burger.updateOne("devoured", "true", condition, function(result) {
         if (result.changedRows == 0) {
             // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
