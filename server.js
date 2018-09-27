658834d6ba12a895efc86f93b609e62d9f89f71f
sydneycode <sstaggs12@gmail.com>
2018-09-26 20:37:38 -0700

    created routes for GET, POST, and PUT requests for the burger_controller

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var exphbs = require("express-handlebars");
+var expressHandlebars = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burger_controller.js");
